02f30d2b0fba234e1440684a4eaf6af5c6f42fcf
sigmaconsultant <sigma@email.com>
2018-11-27 14:17:30 +1100

    Added comments for clarity

--- flickr-connector.js
+++ flickr-connector.js
@@ -2,12 +2,24 @@ var request = require('request');
 var fetchJsonp = require('fetch-jsonp');
 var vm = require('vm');
 
+/*
+ * get
+ * Connect to Flickr API and get data
+ * 
+ * @param {string} q: The text to search. Empty string will show all.
+ * @param {string} l: The row limit.
+ * @returns {FeedData} Returns an array of feed json objects.
+ * 
+ */
+
 function get(params) {
     console.log('initializing flickr request');
     return function(req, res, next) {
         console.log('requesting...');
         request('https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=' + params.q, function (error, response, body) {
             if (!error && response.statusCode == 200) {
+
+                // Resolve the jsonp
                 var jsonpRunner = vm.createContext({jsonFlickrFeed: function(r){return r;}});
                 myObject = vm.runInContext(body,jsonpRunner);
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var server = express();
 var flickrConnector = require('./flickr-connector');
 
+// Enable cors
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -11,9 +12,12 @@ server.use(function(req, res, next) {
 // GET method route
 server.get('/api/getFeed', function (req, res) {
     console.log('received request', req.param("q"));
+
+    // Get flickr connector module and execute
     flickrConnector.get({ q: req.param("q"), l: req.param("l") }) (req, res, function(result) {
         console.log('got result', result);
         if (!result.error) {
+            // Return flickr feed data with a specific structure
             res.send(result.data.map(d => {return {title: d.title, subtitle: d.description, pictureUrl: d.media.m}}));
         }
         else {
